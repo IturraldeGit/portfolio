7933cae812e3868c5231c3aa314d95cc9e7cf5d9
Daniel Iturralde <daniel_iturralde_b@hotmail.com>
2024-01-02 23:08:50 -0500

    Adding a top menu

---
+++ navigator.js
@@ -0,0 +1,40 @@
+console.log('navigator conected')
+
+proyectMenu.addEventListener('click', () => {
+    location.hash = '#proyects';
+})
+aboutMenu.addEventListener('click', () => {
+    location.hash = '#about';
+})
+backwardsArrow.addEventListener('click', () => {
+    header.classList.remove('--inactive');
+    backwardsArrow.classList.add('--inactive');
+    location.hash = '#home';
+})
+
+window.addEventListener('DOMContentLoaded', navigator, false);
+window.addEventListener('hashchange', navigator, false);
+
+function navigator() {
+    if (location.hash.startsWith('#proyects')) {
+        proyects();
+    } else if (location.hash.startsWith('#about')) {
+        aboutMe();
+    } else {
+        home();
+    }
+}
+
+function home() {
+    header.classList.remove('--inactive')
+}
+
+function aboutMe() {
+    backwardsArrow.classList.remove('--inactive');
+    header.classList.add('--inactive');
+}
+
+function proyects() {
+    backwardsArrow.classList.remove('--inactive');
+    header.classList.add('--inactive');
+}
---
+++ nodes.js
@@ -0,0 +1,9 @@
+console.log('nodes conected')
+
+const header = document.querySelector('.header');
+
+const proyectMenu = document.querySelector('.proyects');
+
+const aboutMenu = document.querySelector('.about');
+
+const backwardsArrow = document.querySelector('.menu-backwardsarrow')
